20e4cd2f2ef57c155bc5806df4b3639a7eb3a965
CGTrials <chris13ap@gmail.com>
2025-01-09 14:30:17 +0100

    Got the character counter to work!

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     path_to_file = "books/frankenstein.txt"
     text = book_text(path_to_file)
     number = word_count(text)
+    letters = letter_count(text)
     print(f"There are {number} words found in document")
+    print(letters)
 
 def book_text(path_to_file):
     with open(path_to_file) as f:
@@ -12,5 +14,15 @@ def word_count(text):
     words = text.split()
     return len(words)
     
+def letter_count(text):
+    letter_total = {}
+    lower_text = text.lower()
+    for word in lower_text:
+        for letter in word:
+            if letter in letter_total:
+                letter_total[letter] += 1
+            else:
+                letter_total[letter] = 1
+    return letter_total
 
 main()
